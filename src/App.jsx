302cf5a43001c0f3ce8e33f65559b0f923af1fde
Muhammad-Faisal-FTA <mfaisal1947fort@gmail.com>
2024-10-09 16:14:44 +0500

    input field is updated

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
            setEeather(fr)
            setTimeout(()=>{
               setLoading(false)
+              setCity('')
              },100)
             }
           })
