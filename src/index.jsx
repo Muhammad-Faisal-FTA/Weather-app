0e7930e0fb482e36bbded11debbfd68950806583
Muhammad-Faisal-FTA <mfaisal1947fort@gmail.com>
2024-11-16 17:24:32 +0500

    replaced application version

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Weather from './Weather';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App className="bg-red-800"/>
+  <React.StrictMode className>
+    <Weather className=""/>
   </React.StrictMode>
 );
 
